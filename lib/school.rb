5a7b5c90bf7fef37b07d0347b190f0eaf0503bd8
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-17 19:40:56 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -18,7 +18,7 @@ class School
   def sort  
     sorted = {} 
     roster.each do |grade, students| 
-      sorted[grade] = students.sort  Value is the students sorted in ABC order
+      sorted[grade] = students.sort 
     end
     sorted 
   end
