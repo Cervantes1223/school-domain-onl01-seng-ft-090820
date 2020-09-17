dd18b8767847c3a6cd9edca930fb58eb4f9d774b
Bryan Cervantes <bryanandkyle@gmail.com>
2020-09-17 19:32:51 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,7 +1,3 @@
 class School
-  school = School.new("Bayside High School")
-  school.add_student("AC Slater", 9)
-school.add_student("Kelly Kapowski", 10)
-school.add_student("Screech", 11)
-school.roster
+  
 end
